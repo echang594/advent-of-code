da6e1302e273e9ba15e797d56933b6d1c6c1bc97
Eric Chang <55172722+echang594@users.noreply.github.com>
2024-12-06 23:11:44 -0800

    2024 Day 7

---
+++ 2024/Day07/part1.py
@@ -0,0 +1,22 @@
+with open("2024/Day07/input.txt", "r") as f:
+    lines = f.read().splitlines()
+
+
+def calc(i, total):
+    global val, nums
+    if i == len(nums):
+        return total == val
+    if total > val:
+        return False
+    return calc(i + 1, total + nums[i]) or calc(i + 1, total * nums[i])
+
+
+ans = 0
+for line in lines:
+    val, nums = line.split(":")
+    val = int(val)
+    nums = [int(x) for x in nums.split()]
+    if calc(1, nums[0]):
+        ans += val
+
+print(ans)
